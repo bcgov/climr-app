59a12a1180cd4dcb3f8688f986adac84b5bbdb99
Grice <tirion.grice@gov.bc.ca>
2025-07-03 16:28:32 -0700

    fixed welcome modal html

--- app.R
+++ app.R
@@ -631,11 +631,12 @@ shiny::shinyApp(
     showModal(
       modalDialog(
         title = "Welcome to the climr App!",
-        paste("The climr App is a graphical interface for <a href='https://bcgov.github.io/climr/' target='_blank'>climr</a>, 
+        HTML("The climr App is a graphical interface for <a href='https://bcgov.github.io/climr/' target='_blank'>climr</a>, 
               an R package that provides downscaled climate data for North America. 
               This app allows users to download custom climate data for their areas of interest. 
               It also provides visualizations of the climates of North America and how they are changing." ),
-        easyClose = TRUE
+        easyClose = TRUE,
+        size = "l"
       )
     )
     
