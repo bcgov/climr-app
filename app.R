01d95a24374689774ba4c6747d42e7341c132d7b
Colin Mahony <colin.mahony@gov.bc.ca>
2025-07-04 10:59:15 -0700

    minor

--- app.R
+++ app.R
@@ -631,10 +631,12 @@ shiny::shinyApp(
     showModal(
       modalDialog(
         title = "Welcome to the climr App!",
-        HTML("The climr App is a graphical interface for <a href='https://bcgov.github.io/climr/' target='_blank'>climr</a>, 
+        paste("This is a graphical interface for <a href='https://bcgov.github.io/climr/' target='_blank'>climr</a>, 
               an R package that provides downscaled climate data for North America. 
               This app allows users to download custom climate data for their areas of interest. 
-              It also provides visualizations of the climates of North America and how they are changing." ),
+              It also provides visualizations of the climates of North America and how they are changing.
+              All content of this app is for information only and does not represent BC government policy." 
+        ),
         easyClose = TRUE,
         size = "l"
       )
