11048a0c755ea812d66263754ef01892c91b00dc
Grice <tirion.grice@gov.bc.ca>
2025-07-09 11:20:10 -0700

    minor changes

--- app.R
+++ app.R
@@ -643,7 +643,7 @@ shiny::shinyApp(
     session$allowReconnect("force")
     source("server/getdata_server.R", local = TRUE)
     source("server/visualization_server.R", local = TRUE)
-    user_id <<- uuid()
+    user_id <<- uuid::UUIDgenerate()
     
     showModal(
       modalDialog(
